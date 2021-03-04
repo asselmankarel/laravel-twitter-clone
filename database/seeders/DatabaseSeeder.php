6e9ff5a3f686770f7271d45f1c648f66cce3801e
Karel Asselman <asselman.karel@gmail.com>
2021-03-04 21:00:55 +0100

    added database seeding refactored

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use \App\Models\User;
+use \App\Models\Tweet;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      *
@@ -14,13 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'karel',
             'email' => 'asselman.karel@gmail.com',
             'password' => Hash::make('123456')
         ]);
 
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'test',
             'email' => 'test@gmail.com',
             'password' => Hash::make('123456')
@@ -28,8 +31,8 @@ class DatabaseSeeder extends Seeder
 
         $faker = \Faker\Factory::create();
 
-        \App\Models\Tweet::factory(5)->create([
-            'user_id' => \App\Models\User::where('name', 'karel')->first()->id,
+        Tweet::factory(5)->create([
+            'user_id' => User::where('name', 'karel')->first()->id,
             'body' => $faker->realText($maxNbChars = 200, $indexSize = 2)
         ]);
     }
